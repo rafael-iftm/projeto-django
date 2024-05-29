5f35a200517c202063c332b4028762a65463a181
rafael-devbot <142061101+rafael-iftm@users.noreply.github.com>
2024-05-28 21:00:54 -0300

    feat: django templates (2)

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "recipes"
+
 urlpatterns = [
-    path("", views.home),  # / (Home)
-    path("recipes/<int:id>/", views.recipe),  # /recipes/<int:id>/ (Recipes)
+    path("", views.home, name="home"),  # / (Home) # recipes:home
+    path("recipes/<int:id>/", views.recipe, name="recipe"),  # /recipes/<int:id>/ (Recipes) # recipes:recipe
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from recipes.utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Rafael Prado'
+        'recipes': [make_recipe() for _ in range(10)]
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Rafael Prado'
+        'recipe': make_recipe(),
+        'is_detail_page': True
     })
