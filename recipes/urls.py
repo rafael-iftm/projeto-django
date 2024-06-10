868b34267886f0b3044f31f0b6b802b76bae4808
rafael-devbot <142061101+rafael-iftm@users.noreply.github.com>
2024-06-10 20:15:54 -0300

    feat:  aula 63

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,4 +7,5 @@ app_name = "recipes"
 urlpatterns = [
     path("", views.home, name="home"),  # / (Home) # recipes:home
     path("recipes/<int:id>/", views.recipe, name="recipe"),  # /recipes/<int:id>/ (Recipes) # recipes:recipe
+    path("recipes/category/<int:category_id>/", views.category, name="category"),  # recipes/category/<int:category_id>/ (Category) # recipes:category
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from recipes.utils.recipes.factory import make_recipe
 from recipes.models import Recipe
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
+    })
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name}'
     })
 
 def recipe(request, id):
